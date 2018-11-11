8ba93c496eb3fd0ed19a2dad8492767967b13f11
Jessie Peng <jessie.jie.peng@hotmail.com>
2018-11-11 14:10:51 +1100

    6-9 login and register

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+const cookieParse = require('cookie-parser')
+const userRouter = require('./user')
+
+
+const app = express();
+app.use(cookieParse())
+app.use(bodyParse.json())
+app.use('/user', userRouter)
+
+app.listen(9093, function() {
+    console.log('Node app start at port 9093')
+})
--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { 
+  BrowserRouter, 
+  Route, 
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Login from './container/login/login.js';
+import Register from './container/register/register.js';
+import AuthRoute from './component/authroute/authroute.js';
+import reducers from './reducer.js';
+import './config';
+import './index.css'  
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const reduxDevtools = window.devToolsExtension ? 
+                      window.devToolsExtension() : () => {};
+
+const store = createStore(reducers, compose(
+    applyMiddleware(thunk),
+    reduxDevtools
+));
+
+function Boss() {
+  return <h2>BOSS页面</h2>
+}
+
+
+ReactDOM.render(
+    (<Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <AuthRoute></AuthRoute>
+          <Route path='/boss' component={Boss}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/register' component={Register}></Route>
+        </div>
+      </BrowserRouter>
+    </Provider>),
+    document.getElementById('root')
+    )
+
+    
+
+
+
+
+
+
+
+
